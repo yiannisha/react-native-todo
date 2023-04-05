6c9d18a9eaaa9f3b36663bf9be6904357bc54651
yiannisha <hadjiyiannis.3@gmail.com>
2023-04-06 00:25:57 +0300

    Add global eventsource property

--- contexts/pb.ts
+++ contexts/pb.ts
@@ -1,11 +1,15 @@
 import eventsource from 'react-native-sse'
 import PocketBase from 'pocketbase'
 
+// pocketbase subscriptions need this to work
+// @ts-ignore
+global.EventSource = eventsource
+
 const serverURL = 'https://testdbforyiannis.fly.dev';
 
 export const pb = new PocketBase(serverURL)
 
 export const collections = {
     USERS: 'users',
-    NOTES: 'notes',
+    NOTES: 'todos',
 }
